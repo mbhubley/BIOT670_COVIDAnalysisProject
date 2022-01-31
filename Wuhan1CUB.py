8c7ca36d2dbf13fd9f3988a0dac2e850f57d9899
Kincekara <46578029+Kincekara@users.noreply.github.com>
2022-01-30 22:17:24 -0500

    Create Wuhan1CUB.py
    Pseudocode v1.

---
+++ Wuhan1CUB.py
@@ -0,0 +1,40 @@
+# ------------ PROJECT DEFINITION ------------------
+# 1.	Identify the codon set most used in the Wuhan-1 strain
+# 2.	Do a full count of all codons by AA
+# 3.	Create a usage by AA table (all actual codons used by frequency for that AA)
+#     a.	Look at the distribution of usage throughout the genome, e.g. by plotting the usage frequencies over the genome
+#     b.	Look for the non-highest-frequency codons within the genome
+#     c.	See if these are clustered in regions
+# 4.	Compare to known non-human-pathogenic strain 
+# ---------------------------------------------------
+
+
+
+# get wuhan-1 strain genome sequence (from file or fetch from NCBI)
+
+# get known non-human-pathogenic strain sequence 
+
+
+# define codon:AA dictionary
+
+# define wuhan-1 regions e.g. w1_orf_dic = {region:[start, stop]}
+
+
+# define a fuction for codon count, codon_count()
+
+# define a function for creating an codon usage table, codon_use_table()
+
+# define a fuction for plotting usage frequencies over the genome, codon_freq_plot()
+
+
+
+
+# count codons on wuhan-1
+
+# create codon usage by AA table
+
+# plot codon usage
+
+# find non-highest-frequency codons
+
+# comparison with other strains
